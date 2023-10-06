216a1165cb7e79863b39dd3781b4feaef0aa0965
Amit Hasan Sobuj <AmitHasanICE@gmail.com>
2023-10-06 22:50:31 +0600

    2nd Project

---
+++ thanks.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<?php
+echo strlen("BAUET BAUST BAIUST!");
+echo ("<br>");
+echo strrev("BAUET BAUST BAIUST !");
+echo ("<br>");
+echo str_replace("BAUET ", " BAUST ", " BAUET ARMY BAIUST!");
+echo ("<br>");
+$x = 879.424;
+var_dump(is_int($x));
+echo ("<br>");
+echo strpos("BAUET BAUST BAIUST!"," BAIUST ");
+?>
+</body>
+</html>
